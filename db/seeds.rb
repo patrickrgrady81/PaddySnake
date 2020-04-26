9a255287830c4dd2bec17adc744ac51c6161f639
Patrick Grady <patickrgrady81@gmail.com>
2020-04-26 19:29:15 -0400

    Seed file no longer generates random scores, all scores are live

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,21 +4,21 @@ User.create(username: "Anonymous", email:"anon@pat.com", password:"password")
 User.create(username: "Pat", email:"pat@pat.com", password:"password")
 User.create(username: "a", email:"pat2@pat.com", password:"a")
 
-u = User.find_by(username: "Pat")
-puts "User #{u.email} Created!"
+# u = User.find_by(username: "Pat")
+# puts "User #{u.email} Created!"
 
-# create 10 scores and add them to the user
+# # create 10 scores and add them to the user
 
-4.times do 
-  # random number between 10 and 100
-  r = ((rand * 90) + 10).floor
-  s = Score.create(user_id: u.id, value: r)
-end
+# 4.times do 
+#   # random number between 10 and 100
+#   r = ((rand * 90) + 10).floor
+#   s = Score.create(user_id: u.id, value: r)
+# end
 
-s = Score.create(user_id: u.id, value: 5)
+# s = Score.create(user_id: u.id, value: 5)
 
-puts "#{u.scores.all.count} Scores Created for #{u.email}"
+# puts "#{u.scores.all.count} Scores Created for #{u.email}"
 
-u.scores.all.order("value DESC").each do |score|
-  puts "#{u.email}: #{score.value}"
-end
+# u.scores.all.order("value DESC").each do |score|
+#   puts "#{u.email}: #{score.value}"
+# end
