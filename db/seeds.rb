40563aa85c4143479425a393a51b753189611f58
Patrick Grady <patickrgrady81@gmail.com>
2020-05-16 15:10:36 -0400

    done live

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,18 +4,40 @@ User.create(username: "anonymous", email:"anon@pat.com", password:"password")
 User.create(username: "pat", email:"pat@pat.com", password:"password")
 User.create(username: "a", email:"pat2@pat.com", password:"a")
 User.create(username: "jen", email: "jen@jen.com", password: "jen")
+User.create(username: "z", email: "z", password: "password")
+User.create(username: "mike", email: "mike", password: "password")
 
-# u = User.find_by(username: "Pat")
-# puts "User #{u.email} Created!"
+u = User.find_by(username: "pat")
 
 # # create 10 scores and add them to the user
 
-# 4.times do 
-#   # random number between 10 and 100
-#   r = ((rand * 90) + 10).floor
-#   s = Score.create(user_id: u.id, value: r)
-# end
+4.times do 
+  # random number between 10 and 100
+  r = ((rand * 90) + 10).floor
+  s = Score.create(user_id: u.id, value: r)
+end
 
+u = User.find_by(username: "z")
+puts "User #{u.email} Created!"
+
+# # create 10 scores and add them to the user
+
+2.times do 
+  # random number between 10 and 100
+  r = ((rand * 90) + 10).floor
+  s = Score.create(user_id: u.id, value: r)
+end
+
+u = User.find_by(username: "mike")
+puts "User #{u.email} Created!"
+
+# # create 10 scores and add them to the user
+
+1.times do 
+  # random number between 10 and 100
+  r = ((rand * 90) + 10).floor
+  s = Score.create(user_id: u.id, value: r)
+end
 # s = Score.create(user_id: u.id, value: 5)
 
 # puts "#{u.scores.all.count} Scores Created for #{u.email}"
